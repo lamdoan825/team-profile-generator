801cbd664ddd515250f57ee34fa59fcc931a7b3e
lamdoan825@gmail.com <lam@Lams-Air.attlocal.net>
2021-05-31 01:34:39 -0500

    finished writing methods, fixed syntax errors, and all test pass

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,5 +1,23 @@
-const Employee = require('../lib/Employee.js');
+const Employee = require('../lib/Employee');
 
 test('creates an employee object', () => {
-    const employee = new Employee('John');
+  const employee = new Employee('Jason');
+});
+
+test('set id with constructor', () => {
+  const testValue = 100;
+  const e = new Employee('bob', testValue);
+  expect(e.id).toBe(testValue);
+});
+
+test('set email with constructor', () => {
+  const testValue = 'employee@email.com';
+  const e = new Employee('bob', 1, testValue);
+  expect(e.email).toBe(testValue);
+});
+
+test('getRole() return Employee', () => {
+  const testValue = 'Employee';
+  const e = new Employee('bob', 1, 'employee@email.com');
+  expect(e.getRole()).toBe(testValue);
 });
